9905e024305157fd1b492550a4ea9c0e1f5c77ac
Andriy Kashcha <anvaka@gmail.com>
2018-11-24 00:49:07 -0800

    update placement of the query

--- src/components/QueryInput.vue
+++ src/components/QueryInput.vue
@@ -3,6 +3,7 @@
     <input class='search-input' 
       :value='value' @input="$emit('input', $event.target.value)"
       type='text' :placeholder='placeholder' autofocus ref='input'>
+    <span type='text' class='prefix' ref='prefix'>{{patternPrefix}}</span> 
     <span class='search-measure' ref='measure'></span>
     <span type='text' class='suffix' ref='suffix'>{{patternReminder}}</span> 
   </div>
@@ -18,7 +19,10 @@ export default {
   },
   computed: {
     patternReminder() {
-      return this.pattern.replace('[query]', '');
+      return this.pattern.split('[query]')[1];
+    },
+    patternPrefix() {
+      return this.pattern.split('[query]')[0];
     }
   },
   methods: {
@@ -27,7 +31,7 @@ export default {
     },
     ensureSuffixPos() {
       // Good thing that this doesn't happen often. Otherwise this would be super slow
-      const {suffix, measure} = this.$refs;
+      const {suffix, measure, prefix} = this.$refs;
 
       measure.innerText = this.value;
       let textWidth = this.$refs.measure.getBoundingClientRect();
@@ -35,18 +39,26 @@ export default {
       measure.innerText = this.patternReminder;
       let suffixBox = measure.getBoundingClientRect();
 
+      measure.innerText = this.patternPrefix;
+      let prefixBox = measure.getBoundingClientRect();
+
       let selfBox = this.$el.getBoundingClientRect();
       let maxValue = selfBox.width - suffixBox.width;
 
-      let suffixPos = Math.min(textWidth.right - selfBox.left + 4, maxValue);
-      this.$refs.input.style.paddingRight = suffixBox.width + 'px';
-
+      let suffixPos = Math.min(prefixBox.width + textWidth.width + 4, maxValue);
       if (this.value.length === 0) {
         suffix.style.visibility = 'hidden';
+        prefix.style.visibility = 'hidden';
+        this.$refs.input.style.paddingRight = '0';
+        this.$refs.input.style.paddingLeft = '0';
       } else {
         suffix.style.visibility = 'visible';
+        prefix.style.visibility = 'visible';
+        suffix.style.left = suffixPos + 'px';
+        prefix.style.right = prefixBox.width + 'px';
+        this.$refs.input.style.paddingRight = suffixBox.width + 'px';
+        this.$refs.input.style.paddingLeft = prefixBox.width + 'px';
       }
-      suffix.style.left = suffixPos + 'px';
     }
   },
   mounted() {
@@ -65,9 +77,19 @@ export default {
   }
   .search-measure, .suffix {
     position: absolute;
+    left: 0;
   }
   .suffix {
     right: 0;
+    top: 0;
+    bottom: 0;
+    position: absolute;
+    display: flex;
+    color: #eaaac0;
+  }
+
+  .prefix {
+    left: 0;
     top: 0;
     bottom: 0;
     position: absolute;
